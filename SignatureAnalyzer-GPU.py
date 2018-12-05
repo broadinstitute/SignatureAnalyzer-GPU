592effdb316f89266b9ba4366bccc28bf3427846
amaro@broadinstitute.org <amaro@broadinstitute.org>
2018-12-05 15:32:26 -0500

    pushing input file reading out of class

--- SignatureAnalyzer-GPU.py
+++ SignatureAnalyzer-GPU.py
@@ -387,8 +387,6 @@ def main():
         times = list()
         with tf.Session() as sess:
             sess.run(init_op)
-            print([iter, max_iter])
-            print([deltrack, tol_])
             while deltrack >= tol_ and iter < max_iter:
                 h_new,w_new,lam_new = sess.run([h_, w_,lambda_])
 
@@ -400,8 +398,6 @@ def main():
                 sess.run([gen_W,gen_H,gen_Lambda],feed_dict={h_prime:h_new,w_prime:w_new,lambda_prime:lam_new})
                 deltrack = np.max(np.true_divide(np.abs(lam_new -lam_previous), (lam_previous+1e-5)))
                 lam_previous = lam_new
-                print([iter, max_iter])
-                print([deltrack, tol_])
 
         nonzero_idx = (np.sum(h_new, axis=1) + np.sum(w_new, axis=0)) > active_thresh
         W_active = w_new[:, nonzero_idx]
